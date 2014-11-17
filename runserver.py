471b0a0d100156512c58d62a3417b735037542cf
MaryKatherineMacPherson <kichimk@live.com>
2014-11-17 11:15:08 -0500

    Made HOST a config option
    Externalized configuration of host string- no longer necessary to modify runserver.py file in order to set host='0.0.0.0'

--- runserver.py
+++ runserver.py
@@ -1,5 +1,15 @@
 #! ngssmenv/bin/python
 
-from ngssm import app, connect_db
+from ngssm import app, connect_db, loadconfigs
+from flask import Flask, jsonify, make_response
+from flask.ext.httpauth import HTTPBasicAuth
+from flask.ext.restful import Api
+
 connect_db()
-app.run(debug=True)
+if 'HOST' in app.config:
+  HOST=app.config['HOST']
+else:
+  HOST='127.0.0.1'
+
+app.run(debug=True, host=HOST)
+
